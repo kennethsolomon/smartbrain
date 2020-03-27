c2665c4976f4474dbb5ba46467821fa5bc84cac9
Kenneth Solomon <synecders@gmail.com>
2020-03-27 21:06:38 +0800

    now connecting and fetching user profile to the server and db

--- src/App.js
+++ src/App.js
@@ -34,8 +34,29 @@ export class App extends Component {
       imageURL: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        password: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user:{
+        id: data.id,
+        name: data.name,
+        password: data.password,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
   }
 
   calculateFaceLocation = (data) => {
@@ -64,7 +85,22 @@ export class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageURL: this.state.input })
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if(response){
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries : count }))
+          })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err))
   }
 
@@ -89,17 +125,17 @@ export class App extends Component {
           ?
           <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
             <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
             <FaceRecognition box={box} imageURL={imageURL} />
           </div>
           :(
             route === 'signin' 
-            ? <Signin onRouteChange={this.onRouteChange}/>
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} /> 
             :(
               route === 'signout'
-              ?<Signin onRouteChange={this.onRouteChange}/>
-              :<Register onRouteChange={this.onRouteChange}/>
+              ?<Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              :<Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
             )
           )
         }
